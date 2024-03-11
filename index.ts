021b5accbc671fabf6215de81aab7cfa74326667
Mengran Hao <mengran.hao@studen.ap.be>
2024-03-11 22:39:36 +0000

    New version

--- index.ts
+++ index.ts
@@ -1,8 +1,5 @@
-import { chmod } from "fs";
-import champion from "./champions.json"
-import region from "./regions.json"
+//import champion from "./champions.json"
 import readline from "readline-sync"
-import { exit } from "process";
 
 interface Champions{
     id:string;
@@ -16,7 +13,7 @@ interface Champions{
     role:string;
     race:string;
     abilities:string[];
-    region:string[]
+    region:Regions[]
 }
 
 interface Regions{
@@ -26,38 +23,46 @@ interface Regions{
     regionEmblemUrl:string;
 }
 
-export{}
+async function main() {
+    const response = await fetch(`https://raw.githubusercontent.com/AP-G-1PRO-Webontwikkeling/project-webontwikkeling-MengranHao1998/main/champions.json`);
+    const champion: Champions[] = await response.json();
 
-let terminalapp = true;
-
-do {
-    console.log("Welcome to the JSON data viewer!");
-    console.log(" ");
-    console.log("1. View all data");
-    console.log("2. Filter by ID");
-    console.log("3. Exit");
-    console.log(" ");
-    let answer: number = readline.questionInt('Please enter your choice: ');
-    if (answer === 1) {
-        console.log(champion);
-        console.log(region);
-        terminalapp = false;
-    }
-    else if (answer === 2) {
-        for (let i = 0; i < champion.length; i++) {
-            let championid = champion[i];
-            console.log(championid.id);
-        }
-        let answer2: string = readline.question('Please enter the ID you want to filter by: ');
-        let filterid = champion.filter(champion => champion.id === answer2);
-        console.log(filterid)
-        terminalapp = false;
-    }
-    else if (answer === 3) {
-        terminalapp = false;
-    }
-    else{
-        console.log("This id doesn't exist please try again");
+    do {
+        console.log("Welcome to the JSON data viewer!");
         console.log(" ");
-    }
-} while (terminalapp);
+        console.log("1. View all data");
+        console.log("2. Filter by ID");
+        console.log("3. Exit");
+        console.log(" ");
+        let answer = readline.questionInt('Please enter your choice: ');
+        if (answer === 1) {
+            for (let i = 0; i < champion.length; i++) {
+                console.log(champion[i]);
+            }
+        }
+        else if (answer === 2) {
+            for (let i = 0; i < champion.length; i++) {
+                let championid = champion[i].id;
+                console.log(championid);
+            }
+            let answer2: string = readline.question('Please enter the ID you want to filter by: ');
+            for (let j = 0; j < champion.length; j++) {
+                if (answer2 === champion[j].id) {
+                    let filterid = champion.filter(champion => champion.id === answer2);
+                    console.log(filterid)
+                }
+                else{
+                    console.log("please try again");
+                }
+            }
+        }
+        else if (answer === 3) {
+            break;
+        }
+        else{
+            console.log("This id doesn't exist please try again");
+            console.log(" ");
+        }
+    } while (true);
+}
+main();
